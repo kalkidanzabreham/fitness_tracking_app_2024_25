bc48e18477e14cdaba1df573ad3bdd0a11d8ae60
kalkidan <kalkidanabreham214@gmail.com>
2025-03-14 00:15:49 +0300

    deploy

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: '*', // Allow requests from any origin
+    origin: ['https://fit-taupe-seven.vercel.app'], // Allow requests from any origin
     allowedHeaders: ['Content-Type', 'Authorization'], // Allow the Authorization header
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow necessary methods
   });
